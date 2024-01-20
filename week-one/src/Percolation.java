28cdb84683f922782f2d1360dbab22656daceb1a
Varun Naik <Varunnaik2001@gmail.com>
2024-01-20 21:30:26 +0000

    added open,constructor, isOpen functions

--- week-one/src/Percolation.java
+++ week-one/src/Percolation.java
@@ -1,6 +1,6 @@
 public class Percolation {
     private boolean[][] grid;
-
+    private int openSites;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
         grid = new boolean[n][n];
@@ -9,19 +9,27 @@ public class Percolation {
                 grid[i][j]= false;
             }
         }
+
+        openSites = n*n;
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col){}
+    public void open(int row, int col){
+        if(!grid[row][col]) {
+            grid[row][col] = true;
+            openSites--;
+        }
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){return true;}
+    public boolean isOpen(int row, int col){
+        return grid[row][col];}
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){return true;}
 
     // returns the number of open sites
-    public int numberOfOpenSites(){return 1;}
+    public int numberOfOpenSites(){return openSites;}
 
     // does the system percolate?
     public boolean percolates(){return true;}
